de491a400fbc0189f953d341194a0e30fed72c8d
OE3KUW <72056032+OE3KUW@users.noreply.github.com>
2024-03-20 03:21:42 +0100

    Update serviceSetIdentifier.h
    my neighbours should use his own wifi :-)

--- include/serviceSetIdentifier.h
+++ include/serviceSetIdentifier.h
@@ -4,8 +4,5 @@
                                                 қuran march 2024
 ******************************************************************/
 
-//const char* ssid = "*******";
-//const char* password = "*********************";
-
-const char* ssid = "A1-A82861";
-const char* password = "7PMGDV96J8";
+const char* ssid = "";
+const char* password = "";
